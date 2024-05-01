f4ee57054d31ddc7f63ed0fcfaa49c21ffc72679
Paolo Aliverti <zeppelinmaker@gmail.com>
2024-05-01 07:51:33 +0200

    .
    .

--- Corso-Udemy-TK/TK19/main.py
+++ Corso-Udemy-TK/TK19/main.py
@@ -1,15 +1,21 @@
 import tkinter as tk
 
+def btclic(tbox):
+    txt = tbox.get()
+    print(txt)
+
 app = tk.Tk()
 app.geometry("300x200")
 
 label1 = tk.Label(app, text="Name: ", bg='#c7f9ff')
 label1.place(x=10, y=10)
-tk.Entry(app, bg='#fdffc7').place(x=90, y=10)
+#tk.Entry(app, bg='#fdffc7').place(x=90, y=10)
+name_entry = tk.Entry(app, bg='#fdffc7')
+name_entry.place(x=90, y=10)
 
 tk.Label(app, text="Password: ", bg='#d3ffc7').place(x=10, y=40)
 tk.Entry(app, bg='#fdffc7').place(x=90, y=40)
 
-tk.Button(app, text="Login").place(x=90, y=70)
+tk.Button(app, text="Login", command=lambda: btclic(name_entry) ).place(x=90, y=70)
 
 app.mainloop()
